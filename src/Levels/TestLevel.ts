f2e7692b96c4f4e46a08a970a31d7d3106e3a5f1
PROPHESSOR <xenteze@gmail.com>
2020-01-24 14:57:21 +0200

    [TestLevel] Добавил несколько клеток для демонстрации физики

--- src/Levels/TestLevel.ts
+++ src/Levels/TestLevel.ts
@@ -4,9 +4,9 @@ import Placeful from '../Entities/Placeful';
 export default class TestLevel extends Level {
   playerPosition = 1;
   cells = [
-    ...[0, 0, 1], //
-    ...[1, 0, 0], //
-    ...[1, 0, 0], //
+    ...[0, 3, 1], //
+    ...[1, 0, 2], //
+    ...[1, 3, 2], //
   ];
   brushes = [
     ['EmptyCellBrush', -1],
