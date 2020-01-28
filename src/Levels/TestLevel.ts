81d809bf1bab633586d23ae050ba6dc0e591c885
PROPHESSOR <xenteze@gmail.com>
2020-01-28 13:43:54 +0200

    [TestLevel] Добавлен геймплей

--- src/Levels/TestLevel.ts
+++ src/Levels/TestLevel.ts
@@ -4,12 +4,12 @@ import Placeful from '../Entities/Placeful';
 export default class TestLevel extends Level {
   playerPosition = 1;
   cells = [
-    ...[0, 3, 1], //
+    ...[0, 0, 1], //
     ...[1, 4, 2], //
-    ...[1, 3, 2], //
+    ...[1, 0, 0], //
   ];
   brushes = [
-    ['RotateCellClockwiseBrush', 2],
+    ['RotateCellClockwiseBrush', 1],
     ['RotateCellAntiClockwiseBrush', 2],
   ];
   entities = [[Placeful, []]];
